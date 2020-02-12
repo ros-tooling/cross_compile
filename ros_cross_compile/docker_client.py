2c4b45a3a3fa4d2caa799b08f0f4fec8c56e8997
Emerson Knapp <emerson.b.knapp@gmail.com>
2020-02-12 11:59:06 -0800

    Factor out docker operations into a wrapper client class
    Signed-off-by: Emerson Knapp <emerson.b.knapp@gmail.com>

---
+++ ros_cross_compile/docker_client.py
@@ -0,0 +1,107 @@
+# Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import logging
+from pathlib import Path
+from typing import Dict
+from typing import Optional
+
+import docker
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('Docker Client')
+
+
+class DockerClient:
+    """Simplified Docker API for this package's usage patterns."""
+
+    def __init__(self, nocache):
+        self._client = docker.from_env()
+        self._nocache = nocache
+
+    def build_image(
+        self,
+        dockerfile_dir: Path,
+        dockerfile_name: str,
+        tag: str,
+        buildargs: Optional[Dict[str, str]] = None,
+    ) -> None:
+        """
+        Build a Docker image from a Dockerfile.
+
+        :param dockerfile_dir: Absolute path to directory where Dockerfile can be found.
+        :param dockerfile_name: The name of the Dockerfile to build.
+        :param tag: What tag to give the created image.
+        :param buildargs: Optional dictionary of str->str to set arguments for the build.
+        :return None
+        :raises docker.errors.BuildError: on build error
+        """
+        # Use low-level API to expose logs for image building
+        docker_api = docker.APIClient(base_url='unix://var/run/docker.sock')
+        log_generator = docker_api.build(
+            path=str(dockerfile_dir),
+            dockerfile=dockerfile_name,
+            tag=tag,
+            buildargs=buildargs,
+            quiet=False,
+            nocache=self._nocache,
+            decode=True,
+        )
+        self._process_build_log(log_generator)
+
+    def _process_build_log(self, log_generator) -> None:
+        for chunk in log_generator:
+            # There are two outputs we want to capture, stream and error.
+            # We also process line breaks.
+            error_line = chunk.get('error', None)
+            if error_line:
+                logger.exception(
+                    'Error building Docker image. The follow error was caught:\n%s'.format(
+                        error_line))
+                raise docker.errors.BuildError(error_line)
+            line = chunk.get('stream', '')
+            line = line.rstrip()
+            if line:
+                logger.info(line)
+
+    def run_container(
+        self, image_name: str,
+        environment: Dict[str, str] = {},
+        volumes: Dict[Path, str] = {}
+    ) -> None:
+        """
+        Run a container of an existing image.
+
+        :param image_name: Name of the image to run.
+        :param environment: Map of environment variable names to values.
+        :param volumes: Map of absolute path to a host directory, to str of mount destination.
+        :return None
+        """
+        docker_volumes = {
+            str(src): {
+                'bind': dest,
+                'mode': 'rw',
+            }
+            for src, dest in volumes.items()
+        }
+        container = self._client.containers.run(
+            image=image_name,
+            environment=environment,
+            volumes=docker_volumes,
+            remove=True,
+            detach=True,
+            network_mode='host',
+        )
+        logs = container.logs(stream=True)
+        for line in logs:
+            logger.info(line.decode('utf-8').rstrip())
---
+++ test/test_docker_client.py
@@ -0,0 +1,49 @@
+# Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import docker
+import pytest
+
+from ros_cross_compile.docker_client import DockerClient
+
+
+def test_parse_docker_build_output():
+    """Test the SysrootCreator constructor assuming valid path setup."""
+    # Create mock directories and files
+    client = DockerClient(nocache=False)
+    log_generator_without_errors = [
+        {'stream': ' ---\\u003e a9eb17255234\\n'},
+        {'stream': 'Step 1 : VOLUME /data\\n'},
+        {'stream': ' ---\\u003e Running in abdc1e6896c6\\n'},
+        {'stream': ' ---\\u003e 713bca62012e\\n'},
+        {'stream': 'Removing intermediate container abdc1e6896c6\\n'},
+        {'stream': 'Step 2 : CMD [\\"/bin/sh\\"]\\n'},
+        {'stream': ' ---\\u003e Running in dba30f2a1a7e\\n'},
+        {'stream': ' ---\\u003e 032b8b2855fc\\n'},
+        {'stream': 'Removing intermediate container dba30f2a1a7e\\n'},
+        {'stream': 'Successfully built 032b8b2855fc\\n'},
+    ]
+    # Just expect it not to raise
+    client._process_build_log(log_generator_without_errors)
+
+    log_generator_with_errors = [
+        {'stream': ' ---\\u003e a9eb17255234\\n'},
+        {'stream': 'Step 1 : VOLUME /data\\n'},
+        {'stream': ' ---\\u003e Running in abdc1e6896c6\\n'},
+        {'stream': ' ---\\u003e 713bca62012e\\n'},
+        {'stream': 'Removing intermediate container abdc1e6896c6\\n'},
+        {'stream': 'Step 2 : CMD [\\"/bin/sh\\"]\\n'},
+        {'error': ' ---\\COMMAND NOT FOUND\\n'},
+    ]
+    with pytest.raises(docker.errors.BuildError):
+        client._process_build_log(log_generator_with_errors)
