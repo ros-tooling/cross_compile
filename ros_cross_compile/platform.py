b533b934fd8535fcce4eb1f1dde54e3633187d8f
Emerson Knapp <emerson.b.knapp@gmail.com>
2020-02-13 20:40:12 -0800

    Remove unused function
    Signed-off-by: Emerson Knapp <emerson.b.knapp@gmail.com>

--- ros_cross_compile/platform.py
+++ ros_cross_compile/platform.py
@@ -119,8 +119,3 @@ class Platform:
     def target_base_image(self) -> str:
         """Name of the base OS Docker image for the target architecture."""
         return self._docker_target_base
-
-    @property
-    def native_base_image(self) -> str:
-        """Name of the base OS Docker image for the host architecture."""
-        return self._docker_native_base
