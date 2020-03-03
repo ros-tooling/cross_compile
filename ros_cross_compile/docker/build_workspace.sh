1c6dea166e3165604775e6732a40e00f35f60d9a
Emerson Knapp <emerson.b.knapp@gmail.com>
2020-03-02 21:47:55 -0800

    Reenable -ux bash args in build workspace script
    Signed-off-by: Emerson Knapp <emerson.b.knapp@gmail.com>

--- ros_cross_compile/docker/build_workspace.sh
+++ ros_cross_compile/docker/build_workspace.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-# No -u because setup.bash for ROS has unset variables
-# No -x because it prints all of setup.bash steps, which are numerous and noisy
-set -eo pipefail
+set -euxo pipefail
 
-[ "${ROS_DISTRO}" ] || (echo "ROSDISTRO var unset" && exit 1)
-[ "${TARGET_ARCH}" ] || (echo "TARGET_ARCH var unset" && exit 1)
-[ "${OWNER_USER}" ] || (echo "OWNER_USER var unset" && exit 1)
-
-# shellcheck source=/dev/null
-source /opt/ros/"${ROS_DISTRO}"/setup.bash
+set +ux
+source "$ros_dir"/setup.bash
+set -ux
 colcon build --mixin "${TARGET_ARCH}"-docker \
   --build-base build_"${TARGET_ARCH}" \
   --install-base install_"${TARGET_ARCH}"
