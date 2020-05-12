2e010f6031434bacc2f3d478dd169e2a553d5302
Emerson Knapp <emerson.b.knapp@gmail.com>
2020-05-12 14:07:44 -0700

    Fix missing spaces between words in multiline strings and clean up wording a little
    Signed-off-by: Emerson Knapp <emerson.b.knapp@gmail.com>

--- ros_cross_compile/dependencies.py
+++ ros_cross_compile/dependencies.py
@@ -91,7 +91,7 @@ def assert_install_rosdep_script_exists(
 ) -> bool:
     install_rosdep_script_path = ros_workspace_dir / rosdep_install_script(platform)
     if not install_rosdep_script_path.is_file():
-        raise RuntimeError('Rosdep installation script has never been created,'
-                           'you need to run this without skipping rosdep collection'
-                           'at least once.')
+        raise RuntimeError(
+            'Rosdep installation script has never been created, you need to run this without '
+            'skipping rosdep collection at least once.')
     return True
