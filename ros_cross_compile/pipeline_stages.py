07cd4912cbf305462684d483d17310c019b7021f
Siddharth Gollapudi <sgollapu@amazon.com>
2020-07-09 18:27:51 -0700

    Add the remainder of the stages to the pipeline runner
    Signed-off-by: Siddharth Gollapudi <sgollapu@amazon.com>

--- ros_cross_compile/pipeline_stages.py
+++ ros_cross_compile/pipeline_stages.py
@@ -41,6 +41,10 @@ class PipelineStage(ABC):
     def __init__(self, name):
         self._name = name
 
+    @property
+    def name(self):
+        return self._name
+
     @abstractmethod
     def __call__(self, platform: Platform, docker_client: DockerClient, ros_workspace_dir: Path,
                  customizations: PipelineStageConfigOptions):
